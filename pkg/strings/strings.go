2d346dc881d45c68f829cc30802d9fd098e876be
heyijun <heyijun@sensesw.com>
2023-04-19 13:48:49 +0800

    update strings.go add redirect url match hostname

--- pkg/strings/strings.go
+++ pkg/strings/strings.go
@@ -1,8 +1,23 @@
 package strings
 
+import (
+	"net/url"
+)
+
 func Contains(list []string, needle string) bool {
 	for _, item := range list {
 		if item == needle {
+			return true
+		}
+	}
+	return false
+}
+
+func ContainDomain(list []string, needle string) bool {
+	needleUrl, _ := url.Parse(needle)
+	for _, item := range list {
+		itemUrl, _ := url.Parse(item)
+		if itemUrl.Hostname() == needleUrl.Hostname() {
 			return true
 		}
 	}
